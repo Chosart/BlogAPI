273c052c2bcb91bcce54d9d0ce40815df9a8567d
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-30 22:48:01 +0200

    Added User model

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BlogAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+    }
+}
