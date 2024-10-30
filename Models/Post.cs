423b187600ce65abf849910260b5d61f146e48db
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-30 22:53:08 +0200

    Added Post model

---
+++ Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
