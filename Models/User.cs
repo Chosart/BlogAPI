7aaf06b6bd4e6b984951bc47f07655e1f10cb2e8
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-31 23:02:33 +0200

    Added Hashed Password

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace BlogAPI.Models
@@ -21,5 +23,43 @@ namespace BlogAPI.Models
         [Required]
         [DataType(DataType.Password)]
         public string PasswordHash { get; set; }
+
+        public string Salt { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+
+        [JsonIgnore]
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+        [JsonIgnore]
+        public ICollection<Follower> Followers { get; set; } = new List<Follower>();
+
+        [JsonIgnore]
+        public ICollection<Follower> Following { get; set; } = new List<Follower>();
+
+        [JsonIgnore]
+        public ICollection<Like> Likes { get; set; } = new List<Like>();
+
+        public void SetPassword(string password)
+        {
+            Salt = Guid.NewGuid().ToString();
+
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + Salt));
+                PasswordHash = Convert.ToBase64String(bytes);
+            }
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + Salt));
+                var hash = Convert.ToBase64String(bytes);
+                return hash == PasswordHash;
+            }
+        }
     }
 }
