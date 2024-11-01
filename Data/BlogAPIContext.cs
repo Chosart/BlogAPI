ce6d47a3b374fb5283ce73820a98987c03e9fe5c
Chosart <164737726+Chosart@users.noreply.github.com>
2024-11-01 22:02:51 +0200

    Steps to add dbcontext

---
+++ Data/BlogAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Data
+{
+    public class BlogAPIContext : DbContext
+    {
+        public BlogAPIContext(DbContextOptions<BlogAPIContext> options) : base(options)
+        {
+
+        }
+    }
+}
