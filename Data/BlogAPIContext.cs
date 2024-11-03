e5bf452bf750b256868f161de82c7f03c46a2a93
Chosart <164737726+Chosart@users.noreply.github.com>
2024-11-03 21:50:48 +0200

    Added dependecies to User

--- Data/BlogAPIContext.cs
+++ Data/BlogAPIContext.cs
@@ -11,7 +11,45 @@ namespace BlogAPI.Data
         }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Models.Post> Posts { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Follower> Folowers { get; set; }
+        public DbSet<Like> Likes { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Налаштування для User
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.UserLikes)
+                .WithOne(ul => ul.User)
+                .HasForeignKey(ul => ul.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Comments)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.FollowerUser)
+                .WithOne(f => f.FollowerUser)
+                .HasForeignKey(f => f.FollowerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.FollowingUser)
+                .WithOne(f => f.FollowingUser)
+                .HasForeignKey(f => f.FollowingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Likes)
+                .WithOne(l => l.User)
+                .HasForeignKey(l => l.UserId);
+        }
     }
 }
