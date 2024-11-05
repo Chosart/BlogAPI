f8010eefa59099d7d13b7c4fbe8431b701b5f357
Chosart <164737726+Chosart@users.noreply.github.com>
2024-11-05 22:03:57 +0200

    Initilization ctor for UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using BlogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly BlogAPIContext _context;
+        public UserController(BlogAPIContext context)
+        {
+            _context = context;
+        }
+    }
+}
