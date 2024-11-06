3a790d4c36b03f5a9089201784b1a334422197fc
Chosart <164737726+Chosart@users.noreply.github.com>
2024-11-06 22:49:03 +0200

    Added Create user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogAPI.Data;
+using BlogAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -32,6 +33,25 @@ namespace BlogAPI.Controllers
                 return NotFound("User not found");
             }
             return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser (User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(user.PasswordHash) || string.IsNullOrEmpty(user.Salt))
+            {
+                return BadRequest("PasswordHash and Salt are required");
+            }
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(User), new { id = user.Id }, user);
         }
     }
 }
